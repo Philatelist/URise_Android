6f5b20804c34075c4a07b8945bc2932e3d7759e5
Alexander Usov <a.usoff@gmail.com>
2016-03-01 17:01:19 +0200

    Solved task01 after second check from teacher. Add new methods (Override). Add new test in second class.

--- src/main/java/Task01_Giperbola.java
+++ src/main/java/Task01_Giperbola.java
@@ -12,6 +12,11 @@ public class Task01_Giperbola {
     private double axisA = 5;
     private double axisB = 5;
 
+    public Task01_Giperbola(double axisA, double axisB) {
+        this.axisA = axisA;
+        this.axisB = axisB;
+    }
+
     public void setAxisA(double axisA) {
         this.axisA = axisA;
     }
@@ -24,21 +29,59 @@ public class Task01_Giperbola {
 
         this.pointX = Math.sqrt(Math.pow(axisA, 2) * (1 + (Math.pow(y, 2) / Math.pow(axisB, 2))));
 
-        System.out.println("Axis A = " + this.axisA + ";\n"
-                            + "Axis B = " + axisB + ";\n"
-                            + "Point X = " + this.pointX + ";\n"
-                            + "Point Y = " + y + ";");
+//        print(this.pointX, y);
     }
 
     public void getYformX (double x) {
 
         this.pointY = Math.sqrt(Math.pow(axisB, 2) * ((Math.pow(x, 2) / Math.pow(axisA, 2)) - 1));
 
-        System.out.println("Axis A = " + this.axisA + ";\n"
-                + "Axis B = " + axisB + ";\n"
-                + "Point X = " + x + ";\n"
-                + "Point Y = " + this.pointY + ";");
+//        print(x, this.pointY);
     }
 
+    private void print(double x, double y) {
+        System.out.println("Axis A = " + axisA + ";\n"
+                + "Axis B = " + axisB + ";\n"
+                + "Point X = " + x + ";\n"
+                + "Point Y = " + y + ";");
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task01_Giperbola that = (Task01_Giperbola) o;
+
+        if (Double.compare(that.pointX, pointX) != 0) return false;
+        if (Double.compare(that.pointY, pointY) != 0) return false;
+        if (Double.compare(that.axisA, axisA) != 0) return false;
+        return Double.compare(that.axisB, axisB) == 0;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(pointX);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(pointY);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(axisA);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(axisB);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Task01_Giperbola{" +
+                "pointX=" + pointX +
+                ", pointY=" + pointY +
+                ", axisA=" + axisA +
+                ", axisB=" + axisB +
+                '}';
+    }
 }
--- src/main/java/Task01_test.java
+++ src/main/java/Task01_test.java
@@ -1,10 +1,30 @@
 public class Task01_test {
     public static void main(String[] args) {
 
-        Task01_Giperbola test = new Task01_Giperbola();
+        Task01_Giperbola test1 = new Task01_Giperbola(5, 5);
+        Task01_Giperbola test2 = new Task01_Giperbola(5, 5);
+        Task01_Giperbola test3 = test2;
 
-        test.setAxisA(2);
-        test.getXfromY(4.3);
-        test.getYformX(5.4);
+        test1.getXfromY(4.3);
+        test1.getYformX(5.4);
+
+        test2.getXfromY(4.3);
+        test2.getYformX(5.4);
+
+        test3.getXfromY(5.3);
+        test3.getYformX(5.4);
+
+
+        System.out.println("");
+        System.out.println("test1 toString = " + test1.toString());
+        System.out.println("test2 toString = " + test2.toString());
+        System.out.println("test3 toString = " + test3.toString());
+        System.out.println("");
+        System.out.println("test1.equals(test2) = " + test1.equals(test2));
+        System.out.println("test2.equals(test3) = " + test2.equals(test3));
+        System.out.println("");
+        System.out.println("test1 hashCode = " + test1.hashCode());
+        System.out.println("test2 hashCode = " + test2.hashCode());
+        System.out.println("test3 hashCode = " + test3.hashCode());
     }
 }
